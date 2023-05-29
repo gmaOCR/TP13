c83ab6c8c1905b9ae57ab84d006327d5ba18eb75
Greg M <marainigregory@pm.me>
2023-05-29 09:41:44 +0200

    refactorgin view and html by app

--- lettings/views.py
+++ lettings/views.py
@@ -9,7 +9,7 @@ from .models import Letting
 def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'index.html', context)
+    return render(request, 'lettings/index.html', context)
 
 
 # Cras ultricies dignissim purus, vitae hendrerit ex varius non. In accumsan porta nisl id
@@ -27,4 +27,4 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)
